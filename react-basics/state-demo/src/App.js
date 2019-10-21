2ca7d8f07b8b12cf8f6b08723ebe770322142236
Todd Jones <atoddj@gmail.com>
2019-10-21 11:52:15 -0400

    setState example

--- react-basics/state-demo/src/App.js
+++ react-basics/state-demo/src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import Game from './Game';
+// import Game from './Game';
+import Rando from './Rando';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (<div classname="App">
-      <Game />
-    </div>)
+    return (
+      <div className="App">
+        <Rando maxNum={7} />
+      </div>
+    )
   }
 }
 
