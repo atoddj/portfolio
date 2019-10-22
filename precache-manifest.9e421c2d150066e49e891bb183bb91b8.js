95721b03c219b1aa104afb05427c82102b2169e4
Todd Jones <atoddj@gmail.com>
2019-10-22 15:45:10 -0400

    Updates

---
+++ precache-manifest.9e421c2d150066e49e891bb183bb91b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a48a579a6ece8c6715b7baf51f55ab",
+    "url": "/portfolio/react-dice/index.html"
+  },
+  {
+    "revision": "039f18bc0691763f86d0",
+    "url": "/portfolio/react-dice/static/css/main.597651d9.chunk.css"
+  },
+  {
+    "revision": "3968c0a4c26b2361f063",
+    "url": "/portfolio/react-dice/static/js/2.df67ead5.chunk.js"
+  },
+  {
+    "revision": "039f18bc0691763f86d0",
+    "url": "/portfolio/react-dice/static/js/main.4f984d2d.chunk.js"
+  },
+  {
+    "revision": "7598fbcc2236636ab405",
+    "url": "/portfolio/react-dice/static/js/runtime-main.1a17b7bd.js"
+  }
+]);
