51fd5259244037af5250d880d1efd8195716afd8
Todd Jones <atoddj@gmail.com>
2019-10-25 13:53:48 -0400

    Updated hangman logic

--- react-basics/hangman/src/Hangman.js
+++ react-basics/hangman/src/Hangman.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class Hangman extends Component {
+    static defaultProps = {
+        alphabet: ['a', 'b', 'c', 'd']
+    }      
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            guesses: this.formatData(this.props.alphabet)
+         }
     }
+
+    formatData(alphabet) {
+        var arrayOfObjects = alphabet.map((item) => {
+            let temp = {};
+            temp[item] = {guessed: false};
+            return temp;
+        });
+        var newResult = Object.assign({}, ...arrayOfObjects)
+        return newResult;
+    }
+
     render() { 
         return ( 
             <div className="Hangman">
