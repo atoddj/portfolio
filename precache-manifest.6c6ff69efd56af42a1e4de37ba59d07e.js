6a5eb57d5c3109bb0b6d4d54907bbf9459618909
Todd Jones <atoddj@gmail.com>
2019-10-29 14:32:13 -0400

    Updates

--- precache-manifest.6c6ff69efd56af42a1e4de37ba59d07e.js
+++ precache-manifest.6c6ff69efd56af42a1e4de37ba59d07e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "884c13602a0f600b5d0855099830a8c3",
+    "revision": "8e0986fb15599a9c4ebc77fd64ddc314",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "c25d4810b546a69ae604",
-    "url": "/portfolio/static/css/main.ad8dd3d6.chunk.css"
+    "revision": "7f621583c7e19593326a",
+    "url": "/portfolio/static/css/main.20b6b87b.chunk.css"
   },
   {
     "revision": "331fb45e2315eca5c050",
     "url": "/portfolio/static/js/2.52a7ae1d.chunk.js"
   },
   {
-    "revision": "c25d4810b546a69ae604",
-    "url": "/portfolio/static/js/main.15d40707.chunk.js"
+    "revision": "7f621583c7e19593326a",
+    "url": "/portfolio/static/js/main.f2d605bf.chunk.js"
   },
   {
     "revision": "30a92afd78bc7404b0c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4377a093b15875a02eaa65a1973140de.js"
+  "/portfolio/precache-manifest.6c6ff69efd56af42a1e4de37ba59d07e.js"
 );
 
 self.addEventListener('message', (event) => {
