ad16f88a39926374edce12a0c72273d9e3381c55
Todd Jones <atoddj@gmail.com>
2019-10-31 20:32:53 -0400

    Added logic for toggling lights & neighbor cells

--- react-basics/lights-out/src/Board.js
+++ react-basics/lights-out/src/Board.js
@@ -19,7 +19,7 @@ class Board extends Component {
         let col = 0;
         let gridObj = Array.from({length: this.props.boardSize ** 2})
             .map((el, index) => {
-                let obj = {value: index, light: '', row: row, col: col}
+                let obj = {value: index, light: false, row: row, col: col}
                 col++;
                 if (col>this.props.boardSize-1) { 
                     col=0; 
@@ -38,33 +38,24 @@ class Board extends Component {
          !this.state.boardGenerated && this.randomClick();
          return board;
     }
+
     handleClick(e) {
-        // 5x5 grid example
-        //top row
-        //corners 
-        // if 0, toggle 0, 1 and 5
-        // if 1, toggle 1, 0, 2, and 6
-        // if 2, toggle 2, 1, 3, and 7
-        // if 3, toggle 3, 2, 4, and 8
-        // if 4, toggle 4, 3, and 9
-
-        //second row
-        // if 5, toggle 5, 6, 0 and 10
-        // if 6, toggle 6, 5, 7, 1, and 11
-        // if 7, toggle self, self-1, self+1, self-gridSize, self+gridSize
-
-        /*----------------
-        calculate columns and rows based on grid size
-
-        numRows = gridSize;
-        numCols = gridSize;
-
-        --------------------*/
         let number = Number(e.target.getAttribute('number'));
-        let light = this.state.grid.find(item => item.value === number).light === '' ? 'on' : '';
+        let coords = this.state.grid.find(item => item.value === number);
+        let coordsArr = [];
+        coordsArr.push([coords.row, coords.col]);
+        if(coords.col > 0) { coordsArr.push([coords.row, coords.col-1]) }
+        if(coords.col < this.props.boardSize-1) { coordsArr.push([coords.row, coords.col+1]) }
+        if(coords.row > 0) { coordsArr.push([coords.row-1, coords.col]) }
+        if(coords.row < this.props.boardSize-1) { coordsArr.push([coords.row+1, coords.col]) }
+        coordsArr.forEach((item) => {
+            this.toggleLight(item[0], item[1])
+        })
+    }
 
-        this.setState(state => ({
-            grid: state.grid.map((item) => (item.value === number ? {...item, light: light} : item))
+    toggleLight(x,y) {
+        this.setState(prevState => ({
+            grid: prevState.grid.map((item) => (item.row === x && item.col === y ? {...item, light: !item.light} : item))
         }))
     }
 
--- react-basics/lights-out/src/Cell.js
+++ react-basics/lights-out/src/Cell.js
@@ -4,7 +4,7 @@ import './Cell.css';
 class Cell extends Component {
     render() { 
         return ( 
-            <div className={"Cell " + this.props.light} number={this.props.number} onClick={this.props.handleClick}>
+            <div className={"Cell " + (this.props.light && 'on')} number={this.props.number} onClick={this.props.handleClick}>
             </div>
         );
     }
