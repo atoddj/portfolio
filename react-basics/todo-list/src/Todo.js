ad0667de85da5d3591301063b4024da276e5e033
Todd Jones <atoddj@gmail.com>
2019-11-03 16:29:25 -0500

    Added toggling of completion status

--- react-basics/todo-list/src/Todo.js
+++ react-basics/todo-list/src/Todo.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './Todo.css';
 
 class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { completed: false };
+        this.markDone = this.markDone.bind(this);
+    }
+
+    markDone() {
+        let prevState = this.state.completed;
+        this.setState({
+            completed: !prevState
+        })
+    }
     render() { 
         return ( 
-            <div className="Todo">
+            <div className={"Todo" + (this.state.completed ? " completed": '')} onClick={this.markDone}>
                 {this.props.task}
             </div>
          );
