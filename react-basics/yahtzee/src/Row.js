1ba45f5a5d5e52c8648aed8af9b3afc2de8c5510
Todd Jones <atoddj@gmail.com>
2019-11-11 14:57:02 -0500

    Updated row component for scoring

--- react-basics/yahtzee/src/Row.js
+++ react-basics/yahtzee/src/Row.js
@@ -4,11 +4,18 @@ class Row extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
+        this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick() {
+        const {value} = this.props
+        this.props.scoreFn(value);
+    }
+
     render() {
         const {name, score} = this.props; 
         return ( 
-            <div className="row">
+            <div className="row" onClick={this.handleClick}>
                 <div>{name}</div>
                 <div>{score}</div>
             </div>
