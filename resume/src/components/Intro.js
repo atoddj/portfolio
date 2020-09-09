b2d51810abf65f5453faa35f7d1ee0a9c6bfc1b1
Todd Jones <atoddj@gmail.com>
2020-09-09 09:32:13 -0400

    Added general info to intro

--- resume/src/components/Intro.js
+++ resume/src/components/Intro.js
@@ -6,7 +6,14 @@ const Intro = () => (
   <div className="Intro">
     <div className="Intro-item">
       <h1>Todd Jones</h1>
-      <p>I'm a web developer with 5 years professional experience. I'm driven to make the Internet a more accessible place.</p>
+      <p>I'm a web developer with 5 years professional experience, driven to make the Internet a more accessible place.</p>
+      <h2>General</h2>
+      <p>
+        Princeton, West Virginia <br/>
+        atoddj@gmail.com <br/>
+        Born in 88
+      </p>
+      <p></p>
     </div>
     <div className="Intro-item">
       <img src={photo} className="Photo" alt="Head shot of Todd Jones"/>
