17b5c1a716bffec33c90718c6629926fce135dbd
Todd Jones <atoddj@gmail.com>
2020-09-09 08:46:55 -0400

    Added header layout

--- resume/src/components/App.js
+++ resume/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 const App = () => (
   <div className="App">
-    Hello
+    <Header />
   </div>
 )
 
---
+++ resume/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <div className="Header">
+    <h2>Resume</h2>
+    <button>Download CV</button>
+    <button>Discover</button>
+  </div>
+)
+
+export default Header;
