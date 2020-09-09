423383d0fbc96a6c17d8fdf7f6504ee20ae4d753
Todd Jones <atoddj@gmail.com>
2020-09-09 10:01:08 -0400

    Added social component

--- resume/src/components/App.js
+++ resume/src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Intro from './Intro';
+import Social from './Social';
 
 const App = () => (
   <div className="App">
     <Header />
     <Intro />
+    <Social />
   </div>
 )
 
---
+++ resume/src/components/Social.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Social.css';
+
+const Social = () => (
+  <div className="Social">
+    <div className="icons">
+      <a href="https://github.com/atoddj" target="_blank">
+        <i className="fab fa-github-square"></i>
+      </a>
+      <a href="https://twitter.com/toddjones" target="_blank">
+        <i className="fab fa-twitter-square"></i>
+      </a>
+      <a href="https://linkedin.com/in/atoddj" target="_blank">
+        <i className="fab fa-linkedin"></i>
+      </a>
+    </div>
+    <p>My Social Profiles</p>
+  </div>
+)
+
+export default Social;
